6b5ee93fcbcf3e85e3d6fb9c9822edcaddc01fb0
Alphonso Bradham <bradhamalphonso@gmail.com>
2022-12-01 22:42:50 -0500

    Day 1 part 2

--- Day1/answer.py
+++ Day1/answer.py
@@ -1,14 +1,20 @@
 f = open("input.txt", "r")
-max_num = float('-inf')
+max_nums = set()
+max_nums.add(-1)
+max_nums.add(-2)
+max_nums.add(-3)
 curr_num = 0
 for line in f:
     line = line.strip()
     if(line == ""):
+        for amnt in max_nums:
+            if curr_num > amnt:
+                max_nums.remove(amnt)
+                max_nums.add(curr_num)
+                break
         curr_num = 0
     else:
         curr_num += int(line)
     
-    if curr_num > max_num:
-        max_num = curr_num
 
-print(max_num)
+print(sum(max_nums))
